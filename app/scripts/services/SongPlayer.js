4ab6a98925acbd838f5964f5da243aa6b708fa35
AEH <escamilla.a@gmail.com>
2016-08-12 13:13:27 -0500

    a 7 services 2 completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -26,19 +26,26 @@
 
              currentSong = song;
           };
+         
+          /**
+          * @function playSong
+          * @desc Plays the file loaded as currentBuzzObject and sets song.playing as true
+          * @param {Object} song
+          */
+          var playSong = function(song) {
+              currentBuzzObject.play();
+              song.playing = true;
+          };
 
           SongPlayer.play = function(song) {
               if (currentSong !== song) {
                   setSong(song);
-                  currentBuzzObject.play();
-                  song.playing = true;
+                  playSong(song);
               } else if (currentSong === song) {
                     if (currentBuzzObject.isPaused()) {
-                        currentBuzzObject.play();
-                        song.playing = true;
-
+                        playSong(song);
                     }
-              } 
+              }
           };
          
           SongPlayer.pause = function(song) {
